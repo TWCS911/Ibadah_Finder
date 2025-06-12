e45dedcac4cd41d0b685a4158010a6101fbd70d2
alyssahasyim123 <132744222+TWCS911@users.noreply.github.com>
2025-06-13 05:29:18 +0700

    Main update final

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,52 @@
+import 'package:fasum/screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fasum/firebase_options.dart';
+import 'package:fasum/screens/cover_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:fasum/theme/theme_provider.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Get the user's saved preference for the theme
+  final prefs = await SharedPreferences.getInstance();
+  final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+
+  runApp(MyApp(isDarkMode: isDarkMode));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  final bool isDarkMode;
+  const MyApp({super.key, required this.isDarkMode});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return ChangeNotifierProvider(
+      create: (context) => ThemeProvider()..setDarkMode(isDarkMode),
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, _) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Ibadah Finder',
+            theme: themeProvider.isDarkMode ? ThemeData.dark() : ThemeData.light(),
+            home: StreamBuilder(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return MainScreen();
+                } else {
+                  return CoverScreen();
+                }
+              },
+            ),
+          );
+        },
       ),
     );
   }
